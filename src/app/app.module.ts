9a26d76489086a027a36884d16a9aaeaf3bb101d
kirankumar242018 <kirankumar242018@gmail.com>
2019-09-20 20:06:06 +0530

    opacity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {DashboardComponent} from './components/dashboard/dashboard.component';
+import { TestComponent } from './components/test/test.component'
 import { MaterialModule } from './material.module';
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { HttpClientModule } from '@angular/common/http';
@@ -11,7 +12,8 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
